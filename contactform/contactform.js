483f4d4b651e0685883e38fdcf7b4115f8964fdb
Shane ONeill <oneill.shanepatrick@gmail.com>
2020-02-28 22:26:53 -0500

    contact

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -94,10 +94,12 @@ jQuery(document).ready(function($) {
       return false; 
     }
     else {
-        var $form = $(this);
-        $.post($form.attr("action"), $form.serialize()).then(function() {
-          alert("Thank you!");
-        });
+        // var $form = $(this);
+
+        $(this).unbind('submit').submit()
+        // $.post($form.attr("action"), $form.serialize()).then(function() {
+        //   alert("Thank you!");
+        // });
     }
   });
 });
